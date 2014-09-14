03c3d47f54031b739f9c6b18670a5fcb6e6e2467
Carlos Vasquez Polanco <carlosslim@gmail.com>
2014-09-14 02:45:01 -0400

    Definición de objeto que manejara las entradas a campo por parte del formulario y el POM con información del proyecto

---
+++ src/main/java/org/devdom/api/amet/Form.java
@@ -0,0 +1,107 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Developers Dominicanos.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.devdom.api.amet;
+
+/**
+ * 
+ * @author Carlos Vasquez Polanco
+ */
+public class Form {
+    
+    private String id;
+    private String name;
+    private String value;
+    private String type;
+    
+    public enum Field{
+
+        ID("id"),
+        NAME("name"),
+        VALUE("value"),
+        TYPE("input");
+
+        private final String id;
+
+        Field(String id){
+            this.id = id;
+        }
+
+        public String getText(){
+            return this.id;
+        }
+    }
+
+    private Form(){
+        throw new AssertionError();
+    }
+
+    public Form(String id) {
+        this.id = id;
+    }
+    
+    public Form(String id, String name, String value, String type) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Form{" + "id=" + id + ", name=" + name + ", value=" + value + ", type=" + type + '}';
+    }
+    
+}
